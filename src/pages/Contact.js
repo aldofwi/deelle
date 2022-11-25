abaeb83e273b2ba06ee52ff6e97f1cd39a2f75d7
Aldo FWI <aldo.fwi@gmail.com>
2022-11-25 16:39:36 -0400

    Contact update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
         <head>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Follow Me</title>
+            <title>Follow Her</title>
             <link rel="stylesheet" href="App.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" />
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
